893d3d48925d42fe88d33c15dabeac57376460e7
Rich G <rich.garbutt@gmail.com>
2015-02-28 13:41:03 +0000

    Add bootstrap menu to header and minor changes to head

--- root/functions.php
+++ root/functions.php
@@ -103,12 +103,17 @@ function {%= prefix %}_scripts() {
 	wp_enqueue_style( '{%= prefix %}-style', get_stylesheet_uri() );
 	/* Add Custom CSS */
 	// wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/public/css/custom.min.css' );
+
 	/* Add Bootstrap JS */
 	wp_enqueue_script( 'script-js', get_template_directory_uri() . '/public/js/script.min.js', array('jquery'), '', true );
+
 	/* Add JS for specific Bootstrap JS Calls */
 	wp_enqueue_script( 'theme-js', get_template_directory_uri() . '/public/js/theme.min.js', array('jquery', 'script-js'), '', true );
+
 	//wp_enqueue_script( '{%= prefix %}-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
 	//wp_enqueue_script( '{%= prefix %}-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
